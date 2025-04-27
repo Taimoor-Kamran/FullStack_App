ba7fce0f2e407321fbe6badd46be2959d4d6a39f
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-27 23:05:14 +0500

    added 8 commit

--- my-app/components/Header.tsx
+++ my-app/components/Header.tsx
@@ -1,8 +1,14 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
   return (
-    
+    <header className='my-10 flex justify-between gap-5'>
+      <Link href={'/'}>BookWise</Link>
+      <ul className='flex flex-row'>
+        <li></li>
+      </ul>
+    </header>
   )
 }
 
