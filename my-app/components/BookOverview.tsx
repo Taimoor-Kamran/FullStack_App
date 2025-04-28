53f98284f8a34a659667a4abf1827028267b427c
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-28 17:30:50 +0500

    added 23 commit

--- my-app/components/BookOverview.tsx
+++ my-app/components/BookOverview.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import Image from "next/image";
 
 const BookOverview = ({
   title,
@@ -10,11 +10,24 @@ const BookOverview = ({
   description,
   color,
   cover,
-}:Book) => {
+}: Book) => {
   return (
     <section className="book-overview">
       <div className="flex flex-1 flex-col gap-5">
         <h1>{title}</h1>
+
+        <div className="book-info">
+          <p>
+            <span className="font-semibold text-light-200">{author}</span>
+          </p>
+          <p>
+            Category: {""}
+            <span className="font-semibold text-light-200">{genre}</span>
+          </p>
+          <div className="flex flex-row gap-1">
+            <Image src="/icons/star.svg" alt="star" width={22} height={22}></Image>
+          </div>
+        </div>
       </div>
     </section>
   );
