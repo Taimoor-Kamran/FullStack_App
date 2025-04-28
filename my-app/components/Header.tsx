f5b36ad4b91eeed9246aedc0f7965b1e675da119
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-28 14:44:54 +0500

    added 14 commit

--- my-app/components/Header.tsx
+++ my-app/components/Header.tsx
@@ -9,18 +9,20 @@ const Header = () => {
 
   return (
     <header className="my-10 flex justify-between gap-5">
-      <Link href={"/"}>BookWise</Link>
-      <li>
-        <Link
-          href="/library"
-          className={cn(
-            "text-base cursor-pointer capitalize",
-            pathname === "/library" ? "text-light-200" : "text-light-100"
-          )}
-        >
-          Library
-        </Link>
-      </li>
+      <Link href="/">BookWise</Link>
+      <ul className="flex flex-row items-center gap-8">
+        <li>
+          <Link
+            href="/library"
+            className={cn(
+              "text-base cursor-pointer capitalize",
+              pathname === "/library" ? "text-light-200" : "text-light-100"
+            )}
+          >
+            Library
+          </Link>
+        </li>
+      </ul>
     </header>
   );
 };
