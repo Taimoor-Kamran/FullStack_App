56b1ca076dbb8fed320aea7a3822e23bbe460567
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-29 14:55:11 +0500

    added 34 commit

--- my-apps/components/BookCover.tsx
+++ my-apps/components/BookCover.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+const variantStyle: Record<BookCoverVariant, string> = {
+  extraSmall: "book-cover_extra_small",
+  small: "book-cover_small",
+  medium: "book-cover_medium",
+  regular: "book-cover_regular",
+  wide: "book-cover_wide",
+};
+
 interface Props {
   className?: string;
   variant: "wide" | "small";
