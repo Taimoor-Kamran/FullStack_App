7d6e48061581a001d8900b980c49171e3b834502
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-30 18:23:07 +0500

    added 43 commit

--- my-apps/components/BookCover.tsx
+++ my-apps/components/BookCover.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import React from "react";
+import BookCoverSvg from "./BookCoverSvg";
 
 type BookCoverVariant = "extraSmall" | "small" | "medium" | "regular" | "wide";
 
@@ -34,7 +35,7 @@ const BookCover = ({
       )}
       style={{ backgroundColor: coverColor }}
     >
-      BOOK SIDE SVG
+      <BookCoverSvg coverColor={coverColor}/>
       <div className="absolute z-10" style={{left: '12%', width: '87.5%', height: "88%"}}>
         <Image src={coverImage} alt="Book cover" fill className="rounded-sm object-fill"></Image>
 
