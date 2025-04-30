d0d3588c125e28a6a838c8c0afa1efdf6ea00a92
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-04-30 18:26:54 +0500

    added 46 commit

--- my-apps/components/BookList.tsx
+++ my-apps/components/BookList.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
+interface Props{
+  title: string;
+  books: Book[];
+  containerClassName: string;
+}
+
 const BookList = ({title, books, containerClassName}: Props) => {
   return (
     <section>
