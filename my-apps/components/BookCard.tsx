5387f3df3dac1696be64f5e5fde33ae5005f965d
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-05-03 18:19:40 +0500

    added 66 commit

--- my-apps/components/BookCard.tsx
+++ my-apps/components/BookCard.tsx
@@ -1,31 +1,30 @@
-import React from "react";
 import Link from "next/link";
 import BookCover from "@/components/BookCover";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import { Button } from "./ui/button";
 
 const BookCard = ({
   id,
   title,
   genre,
-  coverColor,
-  coverUrl,
-  isLoanedBook = false,
+  color,
+  cover,
+  isloanedBook = false,
 }: Book) => (
-  <li className={cn(isLoanedBook && "xs:w-52 w-full")}>
+  <li className={cn(isloanedBook && "xs:w-52 w-full")}>
     <Link
       href={`/books/${id}`}
-      className={cn(isLoanedBook && "w-full flex flex-col items-center")}
+      className={cn(isloanedBook && "w-full flex flex-col items-center")}
     >
-      <BookCover coverColor={coverColor} coverImage={coverUrl} />
+      <BookCover coverColor={color} coverImage={cover} variant="wide" />
 
-      <div className={cn("mt-4", !isLoanedBook && "xs:max-w-40 max-w-28")}>
+      <div className={cn("mt-4", !isloanedBook && "xs:max-w-40 max-w-28")}>
         <p className="book-title">{title}</p>
         <p className="book-genre">{genre}</p>
       </div>
 
-      {isLoanedBook && (
+      {isloanedBook && (
         <div className="mt-3 w-full">
           <div className="book-loaned">
             <Image
