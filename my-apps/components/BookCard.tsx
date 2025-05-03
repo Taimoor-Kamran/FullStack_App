fb2d7b313d83fa3ea77024f06e3dbbdb0f61df14
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-05-03 15:44:29 +0500

    added 61 commit

--- my-apps/components/BookCard.tsx
+++ my-apps/components/BookCard.tsx
@@ -11,20 +11,30 @@ const BookCard = ({
   cover,
   isloanedBook = false,
 }: Book) => (
-  <li className={cn(isloanedBook && 'xs:w-52 w-full')}>
-    <Link href={`/books/${id}`} className={cn(isloanedBook && 'w-full flex flex-col items-center')}>
-      <BookCover coverColor={color} coverImage={cover} variant="wide"/>
+  <li className={cn(isloanedBook && "xs:w-52 w-full")}>
+    <Link
+      href={`/books/${id}`}
+      className={cn(isloanedBook && "w-full flex flex-col items-center")}
+    >
+      <BookCover coverColor={color} coverImage={cover} variant="wide" />
 
-      <div className={cn('mt-4', !isloanedBook && 'xs:max-w-40 max-w-28')}>
+      <div className={cn("mt-4", !isloanedBook && "xs:max-w-40 max-w-28")}>
         <p className="book-title">{title}</p>
-            <p className="book-genre">{genre}</p>
+        <p className="book-genre">{genre}</p>
       </div>
 
       {isloanedBook && (
         <div className="mt-3 w-full">
-            <div className="book-loaned">
-                <Image src="/icons/calender.svg" alt="calender" width={18} height={18} className="object-contain"></Image>
-            </div>
+          <div className="book-loaned">
+            <Image
+              src="/icons/calender.svg"
+              alt="calender"
+              width={18}
+              height={18}
+              className="object-contain"
+            />
+            <p className="text-light-100">11 days left to return</p>
+          </div>
         </div>
       )}
     </Link>
