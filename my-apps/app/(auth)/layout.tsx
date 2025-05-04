2cd6c6a10d679aad4d7660d0f3db9216e0334c08
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-05-04 20:08:41 +0500

    added 72 commit

--- my-apps/app/(auth)/layout.tsx
+++ my-apps/app/(auth)/layout.tsx
@@ -1,9 +1,14 @@
-import React, { ReactNode } from 'react'
+import Image from "next/image";
+import React, { ReactNode } from "react";
 
-const layout = ({children}: {children : ReactNode}) => {
-  return (
-    <div>{children}</div>
-  )
-}
+const layout = ({ children }: { children: ReactNode }) => {
+  return <main className="auth-container">
+    <section className="auth-form">
+      <div className="auth-box">
+        <Image src="" alt=""></Image>
+      </div>
+    </section>
+  </main>;
+};
 
-export default layout
+export default layout;
