e7f610a6db2feb159a383b06d2e3d339cb83cac8
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-05-04 23:39:01 +0500

    added 76 commit

--- my-apps/app/(auth)/layout.tsx
+++ my-apps/app/(auth)/layout.tsx
@@ -1,13 +1,13 @@
-import Image from "next/image";
 import React, { ReactNode } from "react";
+import Image from "next/image";
 
-const layout = ({ children }: { children: ReactNode }) => {
+const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <main className="auth-container">
       <section className="auth-form">
         <div className="auth-box">
           <Image src="/icons/logo.svg" alt="logo" width={37} height={37}>
-            <h1 className="text-2xl font-semibold text-white">BookWise</h1>
+            <h1 className="text-2xl font-semibold text-white"></h1>
           </Image>
         </div>
       </section>
@@ -15,4 +15,4 @@ const layout = ({ children }: { children: ReactNode }) => {
   );
 };
 
-export default layout;
+export default Layout;
