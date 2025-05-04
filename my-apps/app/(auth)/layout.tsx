79a0ae080e7c3ca9c578f4be17ea63bef49a195a
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-05-04 23:52:00 +0500

    added 79 commit

--- my-apps/app/(auth)/layout.tsx
+++ my-apps/app/(auth)/layout.tsx
@@ -10,6 +10,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
             <Image src="/icons/logo.svg" alt="logo" width={37} height={37} />
             <h1 className="text-2xl font-semibold text-white">BookWise</h1>
           </div>
+          <div>{children}</div>
         </div>
       </section>
     </main>
