bfd7e5f6d3cd7c97b799634ce5c92f04bda44a6d
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-05-05 00:17:03 +0500

    added 86 commit

--- my-apps/lib/Validations.ts
+++ my-apps/lib/Validations.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const signUpSchema = z.object({
-  fullname: z.string().min(3),
-  email: z.string().email("Invalid email format"),
-  university_id: z.string().min(1, "University ID is required"),
+    fullName: z.string.min(3),
+    email : z.string.email(),
+    universityId: z.coerce.number(),
 });
