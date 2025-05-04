10a2353eb7924c4ba9f3118049712a206be6a6fe
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-05-04 19:24:59 +0500

    added 67 commit

--- my-apps/components/BookCard.tsx
+++ my-apps/components/BookCard.tsx
@@ -12,36 +12,7 @@ const BookCard = ({
   cover,
   isloanedBook = false,
 }: Book) => (
-  <li className={cn(isloanedBook && "xs:w-52 w-full")}>
-    <Link
-      href={`/books/${id}`}
-      className={cn(isloanedBook && "w-full flex flex-col items-center")}
-    >
-      <BookCover coverColor={color} coverImage={cover} variant="wide" />
-
-      <div className={cn("mt-4", !isloanedBook && "xs:max-w-40 max-w-28")}>
-        <p className="book-title">{title}</p>
-        <p className="book-genre">{genre}</p>
-      </div>
-
-      {isloanedBook && (
-        <div className="mt-3 w-full">
-          <div className="book-loaned">
-            <Image
-              src="/icons/calendar.svg"
-              alt="calendar"
-              width={18}
-              height={18}
-              className="object-contain"
-            />
-            <p className="text-light-100">11 days left to return</p>
-          </div>
-
-          <Button className="book-btn">Download receipt</Button>
-        </div>
-      )}
-    </Link>
-  </li>
+  
 );
 
 export default BookCard;
