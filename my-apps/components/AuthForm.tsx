b804d250a72fe3e4ef44b89a241ddf75acea99d4
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-05-05 05:13:55 +0500

    added 94 commit

--- my-apps/components/AuthForm.tsx
+++ my-apps/components/AuthForm.tsx
@@ -1,10 +1,27 @@
 "use client"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
+import { useForm, UseFormReturn } from "react-hook-form"
+import { z } from "zod"
 
 // interface Props<T extends FieldValues>
 
 const AuthForm = ({ type, schema, defaultValues, onSubmit }: Props) => {
+
+    const form: UseFormReturn = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+          username: "",
+        },
+      })
+     
+      // 2. Define a submit handler.
+      function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+      }
+    }
+
   return <div>AuthForm -- {type}</div>;
 };
 
