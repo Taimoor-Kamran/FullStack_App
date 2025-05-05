be393528eed35766faf013afc012da3df4e21573
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-05-05 16:49:17 +0500

    added 98 commit

--- my-apps/components/AuthForm.tsx
+++ my-apps/components/AuthForm.tsx
@@ -6,9 +6,10 @@ import { Schema, z, ZodType } from "zod"
 interface Props<T extends FieldValues> {
     schema : ZodType<T>;
      defaultValues: T;
-     onSubmit: (data: T)
+     onSubmit: (data: T) => Promise<{success: boolean, error?: string}>
+     type: "SIGN_IN" | "SIGN_UP"
 }
-const AuthForm = ({ type, schema, defaultValues, onSubmit }: Props) => {
+const AuthForm = ({ type, schema, defaultValues, onSubmit }: Props<T>) => {
 
     const form: UseFormReturn = useForm({
         resolver: zodResolver(schema),
