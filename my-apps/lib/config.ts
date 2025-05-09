244b21b1514273912d7dad5e42ec49742da61d69
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-05-09 17:13:57 +0500

    added 135 commit

---
+++ my-apps/lib/config.ts
@@ -0,0 +1,8 @@
+const config = {
+    env : {
+        imagekit : {
+            publickey : process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
+            urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT
+        }
+    }
+}
