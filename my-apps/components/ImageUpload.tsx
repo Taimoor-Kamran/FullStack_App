49b43622912c42e10ec96219bbc3649789b40c43
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-05-10 14:37:53 +0500

    added 147 commit

--- my-apps/components/ImageUpload.tsx
+++ my-apps/components/ImageUpload.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import config from "@/lib/config";
 import {
@@ -9,24 +9,26 @@ import {
   ImageKitContext,
 } from "imagekitio-next";
 
-
-
-
 const authenticator = async () => {
   try {
     const response = await fetch(`${config.env.apiEndPoint}/api/auth/imagekit`);
 
-    if(!response.ok){
+    if (!response.ok) {
       const errorText = await response.text();
 
-      throw new Error(`Request failed with status ${response.status} ${errorText}`)
+      throw new Error(
+        `Request failed with status ${response.status}: ${errorText}`
+      );
     }
+
+    const data = await response.json()
+    const { signature, expire, token} = data 
+
 
   } catch (error: any) {
     throw new Error(`Authenticator request failed: ${error.message}`);
   }
 };
-
 
 const ImageUpload = () => {
   return <div>ImageUpload</div>;
