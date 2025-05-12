f1922aa5a4741a02e2d857d3a3899b0d9c572427
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-05-12 17:19:22 +0500

    added 193 commit

--- my-apps/database/schema.ts
+++ my-apps/database/schema.ts
@@ -8,19 +8,21 @@ import {
   pgEnum,
 } from "drizzle-orm/pg-core";
 
-export const STATUS_ENUM = pgEnum('status', ['PENDING', 'APROVED', 'REJECTED'])
+export const STATUS_ENUM = pgEnum("status", ["PENDING", "APROVED", "REJECTED"]);
 
-export const ROLE_ENUM = pgEnum('role', ['USER', 'ADMIN'])
+export const ROLE_ENUM = pgEnum("role", ["USER", "ADMIN"]);
 
-export const BORROW_STATUS_ENUM = pgEnum('borrow_status' ,['BORROWED', 'RETURNED'])
+export const BORROW_STATUS_ENUM = pgEnum("borrow_status", [
+  "BORROWED",
+  "RETURNED",
+]);
 
 export const users = pgTable("users", {
   id: uuid("id").notNull().primaryKey().defaultRandom().unique(),
-  fullName: varchar("full_name", {length: 255}).notNull(),
+  fullName: varchar("full_name", { length: 255 }).notNull(),
   email: text("email").notNull().unique(),
   univeristyId: integer("university_id").notNull().unique(),
-  password: text('password').notNull(),
+  password: text("password").notNull(),
   universityCard: text("university_card").notNull(),
-  status: text()
-
+  status: STATUS_ENUM("status").default("PENDING"),
 });
