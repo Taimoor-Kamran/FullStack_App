800e806708d8fe414e3c7eda9e97ffe1084425a8
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-05-12 16:56:04 +0500

    added 190 commit

--- my-apps/database/schema.ts
+++ my-apps/database/schema.ts
@@ -11,7 +11,8 @@ export const todo = pgTable("todo", {
   id: uuid("id").notNull().primaryKey().defaultRandom().unique(),
   fullName: varchar("full_name", {length: 255}).notNull(),
   email: text("email").notNull().unique(),
-  univeristyId: integer("university_id").notNull(),
+  univeristyId: integer("university_id").notNull().unique(),
   password: text('password').notNull(),
-  universityCard: text("university_card").notNull()
+  universityCard: text("university_card").notNull().unique(),
+
 });
