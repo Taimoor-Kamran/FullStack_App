0f343f592c8ba2bca170659e6b27ddc12b64e7a7
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-05-12 16:52:21 +0500

    added 188 commit

--- my-apps/database/schema.ts
+++ my-apps/database/schema.ts
@@ -9,5 +9,5 @@ import {
 
 export const todo = pgTable("todo", {
   id: uuid("id").notNull().primaryKey().defaultRandom().unique(),
-  fullName: varchar("full_name").notNull(),
+  fullName: varchar("full_name", {length: 255}).notNull(),
 });
