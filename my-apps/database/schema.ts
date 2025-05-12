dda071323736d2f0cabfd0356047b0479884ea22
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-05-12 17:00:40 +0500

    added 191 commit

--- my-apps/database/schema.ts
+++ my-apps/database/schema.ts
@@ -5,14 +5,20 @@ import {
   text,
   boolean,
   pgTable,
+  pgEnum,
 } from "drizzle-orm/pg-core";
 
-export const todo = pgTable("todo", {
+export STATUS_ENUM = pgEnum('status', ['PENDING', 'APROVED', 'REJECTED'])
+
+export ROLE_ENUM = pgEnum('role', ['USER', 'ADMIN'])
+
+export const users = pgTable("users", {
   id: uuid("id").notNull().primaryKey().defaultRandom().unique(),
   fullName: varchar("full_name", {length: 255}).notNull(),
   email: text("email").notNull().unique(),
   univeristyId: integer("university_id").notNull().unique(),
   password: text('password').notNull(),
-  universityCard: text("university_card").notNull().unique(),
+  universityCard: text("university_card").notNull(),
+  status: text()
 
 });
