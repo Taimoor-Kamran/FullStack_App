79b3cf828f65b7c483df0c513f87c1f745e67e18
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-05-13 06:28:57 +0500

    added 207 commit

--- my-apps/auth.ts
+++ my-apps/auth.ts
@@ -1,28 +1,33 @@
-import NextAuth from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials" 
-import { users } from "./database/schema"
-import { eq } from "drizzle-orm"
-import { db } from "./database/drizzle"
-
+import NextAuth from "next-auth";
+import {compare} from "bcryptjs"
+import CredentialsProvider from "next-auth/providers/credentials";
+import { users } from "./database/schema";
+import { eq } from "drizzle-orm";
+import { db } from "./database/drizzle";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    session: {
-        strategy: 'jwt',
-    },
-  providers: [CredentialsProvider({
-    async authorize(credentials){
-        if(!credentials?.email || !credentials?.password){
-            return null
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          return null;
         }
         const user = await db
-            .select()
-            .from(users)
-            .where(eq(users.email, credentials.email.toString()))
-            .limit(1)
+          .select()
+          .from(users)
+          .where(eq(users.email, credentials.email.toString()))
+          .limit(1);
 
-            if(user.length === 0) return null
+        if (user.length === 0) return null;
 
-            const isPasswordValid = await compare(credentials.password.toString())
-    }
-  })],
-})
+        const isPasswordValid = await compare(
+          credentials.password.toString(),
+          user[0].password
+        );
+      },
+    }),
+  ],
+});
