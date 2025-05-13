7769cdf714ff00cbb869a723195091ff272c18dc
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-05-13 16:34:52 +0500

    added 226 commit

--- my-apps/app/(auth)/sign-in/page.tsx
+++ my-apps/app/(auth)/sign-in/page.tsx
@@ -1,5 +1,6 @@
-"use client"
+"use client";
 import AuthForm from "@/components/AuthForm";
+import { signInWithCredentials } from "@/lib/actions/auth";
 import { signUpSchema } from "@/lib/Validations";
 import React from "react";
 
@@ -14,7 +15,7 @@ const Page = () => (
       universityId: 0,
       universityCard: "",
     }}
-    onSubmit={() => {}}
+    onSubmit={signInWithCredentials}
   />
 );
 
