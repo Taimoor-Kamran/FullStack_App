3731dc049590e92f9c7cf0de77333cdd778d11b0
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-06-11 16:50:51 +0500

    added 247 commit

--- my-apps/lib/config.ts
+++ my-apps/lib/config.ts
@@ -9,6 +9,7 @@ const config = {
         databaseUrl : process.env.DATABASE_URL!,
         upstash: {
             redisUrl: process.env.UPSTASH_REDIS_URL,
+            redisToken: process.env.UPSTASH_REDIS_TOKEN,
         }
     }
 }
