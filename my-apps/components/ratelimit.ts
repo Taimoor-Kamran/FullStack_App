b0d95af7b12f82c36a87cb38f08c863b1f906986
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-06-11 17:30:27 +0500

    added 254 commit

--- my-apps/components/ratelimit.ts
+++ my-apps/components/ratelimit.ts
@@ -1,12 +1,13 @@
+import redis from "@/database/redis";
 import { Ratelimit } from "@upstash/ratelimit";
-import { Redis } from "@upstash/redis";
 
 
 const ratelimit = new Ratelimit({
-  redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(10, "10 s"),
+  redis,
+  limiter: Ratelimit.fixedWindow(5, "1m"),
   analytics: true,
   prefix: "@upstash/ratelimit",
 });
 
 
+export default ratelimit;
