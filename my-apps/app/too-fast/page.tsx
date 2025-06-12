ab2c24120b491ca24cf478759216040c4dd1b71e
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2025-06-12 17:11:34 +0500

    added 272 commit

--- my-apps/app/too-fast/page.tsx
+++ my-apps/app/too-fast/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <main className='root-container flex min-h-screen flex-col items-center justify-center'>
-      <h1 className='font-bebas-neue text-5xl font-d'>Whoa, Slow Down There, Speedy!</h1>
+      <h1 className='font-bebas-neue text-5xl font-bold'>Whoa, Slow Down There, Speedy!</h1>
     </main>
   )
 }
